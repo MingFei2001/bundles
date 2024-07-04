3ff1ec6f3bf7f49be1be952238f0503a7478ee6c
MingFei2001 <mingfei2001@gmail.com>
2024-07-04 23:17:47 +0800

    added new information to readme file

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # bundles
 Basic Ubuntu &amp; Debian Linux Easy Setup
+
+## Introduction
+
+Bundles is a script that automates installation of useful application for a fresh debian based systems.
+
+### List of software
+1. Basic Installation
+- curl
+- build-essential
+- git
+- tldr
+- vim
+- neofetch
+- duf
+- btop
+- cmatrix
+- fzf
+- ripgrep
+- fdclone
+- cmake 
+- zip/unzip
+- speedtest-cli
+- ranger
+- tty-clock
+- tmux
+
+2. Full installation
+- not completed yet
+
+3. Extra installation
+- not completed yet
+
+## Getting Started
+Copy the following command to use the script
+
+```bash
+git clone https://github.com/MingFei2001/bundles.git
+cd bundles
+chmod +x ./bundles.sh
+sudo ./bundles.sh
+```
--- bundles.sh
+++ bundles.sh
@@ -46,10 +46,10 @@ while  ! $valid ; do
 	read -p "| Please select setup type: " -r setupType
 	
 	case $setupType in
-		1) valid=true; 
+		1) valid=true;
 			echo "| You selected Basic installation.";
+			echo "| -----------------------------";
 			echo " "
-			
 			# ------------------------------------------
 			echo "| installing curl build-essential"
 			apt install curl build-essential -y
@@ -86,8 +86,50 @@ while  ! $valid ; do
 			echo "| Installation complete"
 			echo " ";;
 			# ------------------------------------------
-		2) valid=true; echo "| You selected Full installation.";echo "| -----------------------------";;
-		3) valid=true; echo "| You selected Extras installation.";echo "| -----------------------------";;
+		2) valid=true;
+			echo "| You selected Full installation.";
+			echo "| -----------------------------";
+			echo " "
+			# ------------------------------------------
+			echo "| installing curl build-essential"
+			apt install curl build-essential -y
+			echo " "
+
+			echo "| installing git tldr"
+			apt install git tldr -y
+			echo " "
+
+			echo "| installing vim neofetch duf"
+			apt install vim neofetch duf -y
+			echo " "
+
+			echo "| installing btop cmatrix"
+			apt install btop cmatrix -y
+			echo " "
+
+			echo "| installing fzf ripgrep"
+			apt install fzf ripgrep -y
+			echo " "
+
+			echo "| installing fdclone cmake"
+			apt install fdclone cmake -y
+			echo " "
+
+			echo "| installing speedtest-cli tty-clock"
+			apt install speedtest-cli tty-clock -y
+			echo " "
+
+			echo "| installing ranger tmux"
+			apt install ranger tmux -y
+			echo " "
+
+			echo "| -----------------------------";
+			echo "| Installation complete"
+			echo " ";;
+		3) valid=true;
+			echo "| You selected Extras installation.";
+			echo "| Feature not implemented yet ..."
+			echo "| -----------------------------";;
 		0) valid=true; echo "| Program aborting ...";;
 		*) echo "| Invalid input.";echo "| -----------------------------";;
 	esac
